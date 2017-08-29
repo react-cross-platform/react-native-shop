125acec93c4a9e38b48f5b169b1f1bef59faad0d
Виталик <vitalik.parshin@gmail.com>
2017-08-29 22:16:32 +0300

    Fix PrettyPrice

--- src/modules/cart/utils.ts
+++ src/modules/cart/utils.ts
@@ -1,21 +1,12 @@
 export const prettyPrice = value => {
-    const convertToString = value.toString();
-    const splitValue = convertToString.split("");
-    const valueLength = splitValue.length;
-    switch (splitValue.length) {
-      case 4:
-        splitValue.splice(-3, 0, " ");
-        return  splitValue.join("");
-  
-      case 5:
-        splitValue.splice(-3, 0, " ");
-        return splitValue.join("");
-  
-      case 6:
-        splitValue.splice(3, 0, " ");
-        return splitValue.join("");
-  
-      default:
-        return value;
-    }
+  const convertToString = value.toString();
+  const splitValue = convertToString.split("");
+  const valueLength = splitValue.length;
+
+  if (splitValue.length > 3) {
+    splitValue.splice(-3, 0, " ");
+    return splitValue.join("");
+  } else {
+    return value;
   }
+};
