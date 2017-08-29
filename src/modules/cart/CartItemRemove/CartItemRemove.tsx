280761d45340a2aa09c9d37f02d7cd70935ddbb6
Виталик <vitalik.parshin@gmail.com>
2017-08-29 22:19:33 +0300

    Fix CartItemRemove

--- src/modules/cart/CartItemRemove/CartItemRemove.tsx
+++ src/modules/cart/CartItemRemove/CartItemRemove.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
-import Ripple from "react-native-material-ripple";
+import { Image, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 
+import RippleWithTimeout from "../../../utils/RippleWithTimeout";
 import { ACTION_REMOVE_CART_ITEM } from "../constants";
 
 const styles = StyleSheet.create({
   container: {
-    width: "10%"
+    width: "10%",
+    height: "20%",
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   removeIcon: {
@@ -36,14 +39,18 @@ class CartItemRemove extends React.Component<
   render() {
     const { index } = this.props;
     return (
-      <View style={styles.container}>
-        <Ripple onPress={() => this.removeCartItem(index)}>
-          <Image
-            style={styles.removeIcon}
-            source={require("../../../../images/trash-icon.png")}
-          />
-        </Ripple>
-      </View>
+      <RippleWithTimeout
+        style={styles.container}
+        rippleCentered={true}
+        rippleSize={35}
+        timeOut={200}
+        onPress={() => this.removeCartItem(index)}
+      >
+        <Image
+          style={styles.removeIcon}
+          source={require("../../../../images/trash-icon.png")}
+        />
+      </RippleWithTimeout>
     );
   }
 }
